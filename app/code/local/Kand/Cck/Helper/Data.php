d3e4033d728a6f8a7859b8cfb726df48c2b608bd
Andrew Roslik <andkirby@gmail.com>
2014-05-04 20:19:49 +0300

    - Added base module files.

---
+++ app/code/local/Kand/Cck/Helper/Data.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Class Kand_Cck_Helper_Data
+ */
+class Kand_Cck_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
---
+++ app/code/local/Kand/Cck/data/kand_cck_setup/data-install-0.1.0.php
@@ -0,0 +1,5 @@
+<?php
+/* @var $this Mage_Core_Model_Resource_Setup */
+$this->startSetup();
+
+$this->endSetup();
