28a918665fb4c0fbe431db2f62d83baf54821e43
Andrew Roslik <andkirby@gmail.com>
2014-05-05 01:41:19 +0300

    - The module moved into community (2).

---
+++ app/code/community/Kand/Cck/controllers/Adminhtml/Kand/Cck/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Index controller
+ *
+ * @category   Mage
+ * @package    Mage_Api2
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+class Kand_Cck_Adminhtml_Kand_Cck_IndexController extends Mage_Adminhtml_Controller_Action
+{
+    /**
+     * Show user types grid
+     */
+    public function indexAction()
+    {
+        //some code
+    }
+}
