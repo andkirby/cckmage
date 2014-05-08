4092af603b45f9804534910c02d1bfa79a1ae556
Andrew Roslik <andkirby@gmail.com>
2014-05-08 03:24:06 +0300

    - Updated tests.

--- dev/tests/unit/testsuite/Kand/Cck/Helper/DataTest.php
+++ dev/tests/unit/testsuite/Kand/Cck/Helper/DataTest.php
@@ -66,33 +66,55 @@ class Kand_Cck_Helper_DataTest extends PHPUnit_Framework_TestCase
      */
     public function testCollectTextsSuccess()
     {
-        $this->markTestIncomplete();
         $methods = array('___');
         $test = $this->_getObjectMock($methods);
+
         //@startSkipCommitHooks
         $html = <<<HTML
-<div>
+Some text in the beginning.
+<div class="some-class" style="background-color: #000055">
     <h1>Some Title</h1>
     <span>Span Text</span>
     <a href="#mylink">My pretty nice <em>link</em></a>
     <p>Some long paragraph
-    with a <a>link</a></p>
+    with a <a href="http://example.com/?my=1">link</a></p>
 </div>
+Some text in the ending.
 HTML;
         //@finishSkipCommitHooks
 
-        $result = $test->collectTexts(
-            $html
-        );
+        $result = $test->collectTexts($html);
 
-        $this->assertEquals(
-            array(
-                'Some Title',
-                'Span Text',
-                'My pretty nice <em>link</em>',
-                'Some long paragraph with a <a>link</a>',
-            ),
-            $result
+        //test return in collectTexts() method
+        $this->assertEquals($result, $test->getTexts());
+
+        $cnt = 0;
+        $expected = array(
+            'text_' . ++$cnt => 'Some text in the beginning.',
+            'text_' . ++$cnt => 'Some Title',
+            'text_' . ++$cnt => 'Span Text',
+            'text_' . ++$cnt => 'My pretty nice <em>link</em>',
+            'text_' . ++$cnt => 'Some long paragraph
+    with a <a href="http://example.com/?my=1">link</a>',
+            'text_' . ++$cnt => 'Some text in the ending.',
         );
+        $this->assertEquals($expected, $result);
+
+        //Check output HTML
+        //@startSkipCommitHooks
+        $expected = <<<HTML
+{{cms_text key="text_1"}}
+<div class="some-class" style="background-color: #000055">
+    <h1>{{cms_text key="text_2"}}</h1>
+    <span>{{cms_text key="text_3"}}</span>
+    <a href="#mylink">{{cms_text key="text_4"}}</a>
+    <p>{{cms_text key="text_5"}}</p>
+</div>
+{{cms_text key="text_6"}}
+HTML;
+        //@finishSkipCommitHooks
+
+        $result = $test->getOutputHtml();
+        $this->assertEquals($expected, $result);
     }
 }
